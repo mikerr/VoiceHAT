21674411ef7c269db9fbb7083b5af6a0cabb794f
mikerr <mike@redrobe.net>
2024-12-31 23:10:36 +0000

    respond to date

--- voiceassistant.py
+++ voiceassistant.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 # very simple local voice assistant 
-# just tells the time when asked "whats the time, tel me the time etc"
+# Can ask :
+# "whats the time", "tell me the time"
+# "whats the date"
 
 # pip3 install vosk
 
@@ -24,12 +26,26 @@ def read_question (sentence):
         today = datetime.now()
         currenttime = today.strftime("%H %M")
         answer = "the time is " + currenttime
+    if ("date" in sentence):
+        today = datetime.now()
+        now = today.strftime("%B %d")
+        answer = "the date is " + now
     return answer
+
+def speak(sentence):
+        process = subprocess.Popen(['espeak','-a 50','--stdin'], stdin=subprocess.PIPE )
+        process.stdin.write(sentence.encode('utf-8'))
+        process.stdin.flush()
+        process.stdin.close()
+
+        process.wait()
 
 device_info = sd.query_devices(None, "input")
 samplerate = int(device_info["default_samplerate"])
         
 model = Model(lang="en-us")
+
+speak("hello")
 
 stream = sd.RawInputStream(samplerate=samplerate, blocksize = 8000, 
             dtype="int16", channels=1, callback=callback)
@@ -37,6 +53,7 @@ stream = sd.RawInputStream(samplerate=samplerate, blocksize = 8000,
 print("#" * 80)
 print("Press Ctrl+C to stop the recording")
 print("#" * 80)
+
 
 rec = KaldiRecognizer(model, samplerate)
         
@@ -49,11 +66,8 @@ while True:
         print(question)
         answer = read_question(question)
         if (answer != "") :
+            # stop listening while we are speaking
             stream.stop()
-            process = subprocess.Popen(['espeak','-a 50','--stdin'], stdin=subprocess.PIPE )
-            process.stdin.write(answer.encode('utf-8'))
-            process.stdin.flush()
-            process.stdin.close()
-
-            process.wait()
+            speak(answer)
+            # start listening again
             stream.start()
